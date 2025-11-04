e3fea0e4c519df60d53d114bde568038bf92c53f
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-11-04 23:03:16 +0800

    Update nft_ext_fullcone.c

--- nft-fullcone/src/nft_ext_fullcone.c
+++ nft-fullcone/src/nft_ext_fullcone.c
@@ -121,7 +121,7 @@ static int exp_event_cb(unsigned int events, const struct nf_exp_event *item)
 }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 6, 113)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 6, 114)
 // include/net/netfilter/nf_tables.h validate function signature changed
 static int nft_fullcone_validate(const struct nft_ctx *ctx, const struct nft_expr *expr)
 #else
